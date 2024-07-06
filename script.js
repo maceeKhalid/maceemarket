ea6f7b9961c4afa69ffd62ce331eb1001845c9d4
macee khalid <maceekhalid@gmail.com>
2024-07-05 18:49:38 -0700

    website completed

---
+++ script.js
@@ -0,0 +1,9 @@
+/*const countLabel = document.getElementById('countLabel');
+const resetBtn = document.getElementById('resetBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+const decreaseBtn = document.getElementById('decreaseBtn');
+let count = 0;
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}*/
